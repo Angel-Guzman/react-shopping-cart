5ca0bf2724e45c549bd0c35c00ea1a845f6955ef
Angelg10 <angelguzman97@gmail.com>
2020-05-14 15:40:20 -0400

    fully refactored to Context API

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
 import { ProductContext } from './contexts/ProductContext.js';
-import { CartContext } from './contexts/ProductContext.js';
+import { CartContext } from './contexts/CartContext.js';
 import Product from './components/Product';
 
 function App() {
@@ -23,16 +23,18 @@ function App() {
 	return (
 		<div className="App">
 			<ProductContext.Provider value={{ products, addItem }}>
-				<Navigation cart={cart} />
+				<CartContext.Provider value={{ cart }}>
+					<Navigation />
 
-				{/* Routes */}
-				<Route exact path="/">
-					<Products />
-				</Route>
+					{/* Routes */}
+					<Route exact path="/">
+						<Products />
+					</Route>
 
-				<Route path="/cart">
-					<ShoppingCart cart={cart} />
-				</Route>
+					<Route path="/cart">
+						<ShoppingCart />
+					</Route>
+				</CartContext.Provider>
 			</ProductContext.Provider>
 		</div>
 	);
